489efe07e4c0d1be12541473c2691da7f8f534e1
Mohammad Raghib <khanraghib61@gmail.com>
2025-10-22 02:24:54 +0530

    Add files via upload

---
+++ # MADLIBS GAME ex 3.py
@@ -0,0 +1,31 @@
+# MADLIBS GAME
+# a story game where users provide words to fill in the blanks of a story template
+
+print("Welcome to the Madlibs Game!")
+print("................................")
+print("................................")
+
+adjective1 = input("enter an adjectice(description of something): ")
+noun1 = input("enter a noun (person, place, or thing): ")
+
+
+
+verb1 = input("enter a verb (ening with -ing) ")
+
+
+adjective2 = input("enter another adjective: ")
+adjective3 = input("enter one more adjective :) ;-")
+
+verb2 = input("enter a verb (ending with -ing) ")
+
+print("................................")
+print("................................")
+print("Here is your story: ")
+print("................................")
+print("................................")
+
+print(f"I went to a {adjective1} castle last sunday.")
+print(f"In the castle i saw a {noun1} and it was {verb1}.")
+print(f"The {noun1} was very {adjective2} and {adjective3}")
+print(f"I really got scared of the {noun1} because it started {verb2} towards me") 
+print("So i ran away as fast as i could and never went back to that castle again!")
